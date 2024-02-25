f2dc2ab78a321eeb1d63c80634b76c734ec354b8
TLG <tranlangiac@gmail.com>
2024-02-26 03:58:29 +0700

    Fixing Home page path
    Changing Home page path from ./pages/auth/Home.jsx to ./pages/Home.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
-
-import Home from "./pages/auth/Home";
-
-
+import Home from "./pages/Home";
 
 function App() {
 	return (
